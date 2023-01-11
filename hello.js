222c0b9c1da8abe5909f259beee410110e749dcf
Tahel Aharoni <tahel.aharoni@gmail.com>
2023-01-11 15:07:29 +0200

    updated my status

--- hello.js
+++ hello.js
@@ -1,1 +1,29 @@
-console.log("I did commit");
+const calc=(operand1,operand2,operator)=>{
+    const final=0;
+    const op1=parseInt(operand1);
+    const op2=parseInt(operand2);
+    if(op1==NaN||op2==NaN){
+        return NaN;
+    }
+    if(operator=="+")
+    {
+        return op1+op2;
+    }
+    else if(operator=="-")
+    {
+        return op1-op2;
+    }
+    else if(operator=="*"){
+        return op1*op2;
+    }
+    else if(operator=="/"){
+        return op1/op2;
+    }
+    else{
+        return NaN;
+    }
+}
+
+const compact=(array)=>{
+    
+}
